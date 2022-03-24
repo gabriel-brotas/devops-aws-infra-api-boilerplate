fd9bda8320ff891488d5e0d6a3139113a0e6f113
GabrielBrotas <gabrielbrotas22@gmail.com>
2022-03-24 16:35:44 -0300

    network template

---
+++ README.md
@@ -0,0 +1,7 @@
+
+## How to run 
+
+infra
+```
+aws cloudformation create-stack --stack-name api-boilerplate-infra --capabilities CAPABILITY_IAM --template-body file://./templates/1.network.yml
+```
--- src/server.js
+++ src/server.js
@@ -7,6 +7,10 @@ app.get('/health-check', (req, res) => {
     return res.status(200).json({ success: true })
 })
 
+app.get('/version', (req, res) => {
+    return res.status(200).json({ success: true, version: '1.0.0' })
+})
+
 app.get('/', (req, res) => {
     const env1 = process.env.ENV1;
     const env2 = process.env.ENV2;
@@ -14,7 +18,7 @@ app.get('/', (req, res) => {
     return res.status(200).json({ env1, env2 })
 })
 
-const PORT = 4000
+const PORT = 4000;
 
 app.listen(() => {
     console.log(`Server is running on port ${PORT}`);
